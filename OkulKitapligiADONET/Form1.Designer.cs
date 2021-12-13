f4d5dd20f1211db5d03fbaeeb32ed1e3aea0c3ff
Umut Yılmaz <umut.yilmaz1@mf.karaelmas.edu.tr>
2021-12-13 14:30:38 +0300

    Add project files.

---
+++ OkulKitapligiADONET/Form1.Designer.cs
@@ -0,0 +1,181 @@
+﻿
+namespace OkulKitapligiADONET
+{
+    partial class FormYazarlar
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtYazar = new System.Windows.Forms.TextBox();
+            this.btnEkle = new System.Windows.Forms.Button();
+            this.btnTemizle = new System.Windows.Forms.Button();
+            this.dataGridViewYazarlar = new System.Windows.Forms.DataGridView();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.guncelleToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.silToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.silPasifeCekToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.silBaskaBirYontemToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewYazarlar)).BeginInit();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(22, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(122, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Yazar Ad-Soyad:";
+            // 
+            // txtYazar
+            // 
+            this.txtYazar.Location = new System.Drawing.Point(145, 42);
+            this.txtYazar.Name = "txtYazar";
+            this.txtYazar.Size = new System.Drawing.Size(362, 27);
+            this.txtYazar.TabIndex = 1;
+            // 
+            // btnEkle
+            // 
+            this.btnEkle.BackColor = System.Drawing.Color.SteelBlue;
+            this.btnEkle.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnEkle.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnEkle.ForeColor = System.Drawing.SystemColors.Control;
+            this.btnEkle.Location = new System.Drawing.Point(537, 42);
+            this.btnEkle.Name = "btnEkle";
+            this.btnEkle.Size = new System.Drawing.Size(94, 29);
+            this.btnEkle.TabIndex = 2;
+            this.btnEkle.Text = "EKLE";
+            this.btnEkle.UseVisualStyleBackColor = false;
+            this.btnEkle.Click += new System.EventHandler(this.btnEkle_Click);
+            // 
+            // btnTemizle
+            // 
+            this.btnTemizle.BackColor = System.Drawing.Color.SteelBlue;
+            this.btnTemizle.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnTemizle.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnTemizle.ForeColor = System.Drawing.SystemColors.Control;
+            this.btnTemizle.Location = new System.Drawing.Point(655, 42);
+            this.btnTemizle.Name = "btnTemizle";
+            this.btnTemizle.Size = new System.Drawing.Size(94, 29);
+            this.btnTemizle.TabIndex = 3;
+            this.btnTemizle.Text = "TEMİZLE";
+            this.btnTemizle.UseVisualStyleBackColor = false;
+            this.btnTemizle.Click += new System.EventHandler(this.btnTemizle_Click);
+            // 
+            // dataGridViewYazarlar
+            // 
+            this.dataGridViewYazarlar.AllowUserToAddRows = false;
+            this.dataGridViewYazarlar.AllowUserToDeleteRows = false;
+            this.dataGridViewYazarlar.AllowUserToOrderColumns = true;
+            this.dataGridViewYazarlar.BackgroundColor = System.Drawing.SystemColors.Window;
+            this.dataGridViewYazarlar.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewYazarlar.Location = new System.Drawing.Point(22, 85);
+            this.dataGridViewYazarlar.Name = "dataGridViewYazarlar";
+            this.dataGridViewYazarlar.ReadOnly = true;
+            this.dataGridViewYazarlar.RowHeadersWidth = 51;
+            this.dataGridViewYazarlar.RowTemplate.Height = 29;
+            this.dataGridViewYazarlar.Size = new System.Drawing.Size(756, 353);
+            this.dataGridViewYazarlar.TabIndex = 4;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.guncelleToolStripMenuItem,
+            this.silToolStripMenuItem,
+            this.silPasifeCekToolStripMenuItem,
+            this.silBaskaBirYontemToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(215, 128);
+            // 
+            // guncelleToolStripMenuItem
+            // 
+            this.guncelleToolStripMenuItem.Name = "guncelleToolStripMenuItem";
+            this.guncelleToolStripMenuItem.Size = new System.Drawing.Size(214, 24);
+            this.guncelleToolStripMenuItem.Text = "Güncelle";
+            this.guncelleToolStripMenuItem.Click += new System.EventHandler(this.guncelleToolStripMenuItem_Click);
+            // 
+            // silToolStripMenuItem
+            // 
+            this.silToolStripMenuItem.Name = "silToolStripMenuItem";
+            this.silToolStripMenuItem.Size = new System.Drawing.Size(214, 24);
+            this.silToolStripMenuItem.Text = "Sil";
+            this.silToolStripMenuItem.Click += new System.EventHandler(this.silToolStripMenuItem_Click);
+            // 
+            // silPasifeCekToolStripMenuItem
+            // 
+            this.silPasifeCekToolStripMenuItem.Name = "silPasifeCekToolStripMenuItem";
+            this.silPasifeCekToolStripMenuItem.Size = new System.Drawing.Size(214, 24);
+            this.silPasifeCekToolStripMenuItem.Text = "Pasife Çek";
+            this.silPasifeCekToolStripMenuItem.Click += new System.EventHandler(this.silPasifeCekToolStripMenuItem_Click);
+            // 
+            // silBaskaBirYontemToolStripMenuItem
+            // 
+            this.silBaskaBirYontemToolStripMenuItem.Name = "silBaskaBirYontemToolStripMenuItem";
+            this.silBaskaBirYontemToolStripMenuItem.Size = new System.Drawing.Size(214, 24);
+            this.silBaskaBirYontemToolStripMenuItem.Text = "Sil-Baska bir yontem";
+            this.silBaskaBirYontemToolStripMenuItem.Click += new System.EventHandler(this.silBaskaBirYontemToolStripMenuItem_Click);
+            // 
+            // FormYazarlar
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridViewYazarlar);
+            this.Controls.Add(this.btnTemizle);
+            this.Controls.Add(this.btnEkle);
+            this.Controls.Add(this.txtYazar);
+            this.Controls.Add(this.label1);
+            this.Name = "FormYazarlar";
+            this.Text = "Yazar İşlemleri";
+            this.Load += new System.EventHandler(this.FormYazarlar_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewYazarlar)).EndInit();
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtYazar;
+        private System.Windows.Forms.Button btnEkle;
+        private System.Windows.Forms.Button btnTemizle;
+        private System.Windows.Forms.DataGridView dataGridViewYazarlar;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem guncelleToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem silToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem silPasifeCekToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem silBaskaBirYontemToolStripMenuItem;
+    }
+}
+
---
+++ OkulKitapligiADONET/Form1.cs
@@ -0,0 +1,388 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace OkulKitapligiADONET
+{
+    public partial class FormYazarlar : Form
+    {
+        public FormYazarlar()
+        {
+            InitializeComponent();
+        }
+
+        //Global Alan
+
+        //SQLCONNECTION Nesnesi: SQL veritabanına bağlantı kurmak için kullanacağımız classtır.
+        //System.Data.Client namespace'i içerisinde yer alır.
+
+
+        SqlConnection baglanti = new SqlConnection();
+        string SQLBaglantiCumlesi = @"Server=DESKTOP-TUMHS1A;Database=OKULKITAPLIGI;Trusted_Connection=True;";
+
+        private void FormYazarlar_Load(object sender, EventArgs e)
+        {
+            dataGridViewYazarlar.MultiSelect = false; //Çoklu satır seçimi engellendi
+            dataGridViewYazarlar.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            //fare ile datagrid üzerinde bir hücreye tıkladığında bulunduğu satırı tamamen seçecek
+
+            //dataGridView'e sağ tıklanınca gelecek olan contextmenustrip ataması yapıldı
+            dataGridViewYazarlar.ContextMenuStrip = contextMenuStrip1;
+
+            //Grid'in içine bilgileri getireceğiz
+            TumYazarlariGetir();
+
+
+        }
+
+        private void TumYazarlariGetir()
+        {
+
+            try
+            {
+
+                //SQLCOMMAND nesnesi: Sorgularımızı ve proserdürlerimize ait komutları alan nesnedir.
+                SqlCommand komut = new SqlCommand();
+                komut.Connection = baglanti;
+                komut.CommandType = CommandType.Text;
+                string sorgu = "Select * from Yazarlar where SilindiMi=0 order by YazarID desc";
+                komut.CommandText = sorgu;
+                BaglantiyiAc();
+
+                //DataSQLADAPTER nesnesi, sorgu çalışınca oluşan dataların aktarılması işlemini yapar. Adaptore hangi komut işleneceğini ctor'da verebiliriz ya da daha sonra verebiliriz.
+                //1.Yöntem
+                SqlDataAdapter adaptor = new SqlDataAdapter(komut); // ctor'da komutu verdik
+                //2.Yöntem
+                //SqlDataAdapter adaptor = new SqlDataAdapter();
+                //adaptor.SelectCommand = komut;  //Komutu daha sonradan da verilebilir.
+
+                //Adaptorun içindeki verileri sanalTabloya alıyoruz.
+                DataTable sanalTablo = new DataTable();
+                adaptor.Fill(sanalTablo);
+
+                dataGridViewYazarlar.DataSource = sanalTablo;
+                dataGridViewYazarlar.Columns["SilindiMi"].Visible = false;
+                dataGridViewYazarlar.Columns["YazarAdSoyad"].Width = 220;
+                dataGridViewYazarlar.Columns["KayitTarihi"].Width = 150;
+
+                BaglantiyiKapat();
+
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show($"Beklenmedik bir hata oluştu! HATA: {ex.Message}", "HATA", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnEkle_Click(object sender, EventArgs e)
+        {
+            switch (btnEkle.Text)
+            {
+                case "EKLE":
+                    try
+                    {
+                        if (string.IsNullOrEmpty(txtYazar.Text))
+                        {
+                            MessageBox.Show("Lütfen yazar bilgisini giriniz!", "UYARI", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        }
+                        else
+                        {
+                            //ekleme yapacağız.
+                            string insertCumlesi = $"insert into Yazarlar (KayitTarihi, YazarAdSoyad, SilindiMi) values ('{TarihiDuzenle(DateTime.Now)}','{txtYazar.Text.Trim()}',0) ";
+
+                            SqlCommand insertkomut = new SqlCommand(insertCumlesi, baglanti);
+                            //baglantı açılacak metot çağıralım
+                            BaglantiyiAc();
+                            int sonucum = insertkomut.ExecuteNonQuery();
+                            if (sonucum > 0) //effected rows var
+                            {
+                                MessageBox.Show("Yeni yazar sisteme eklendi");
+                                TumYazarlariGetir();
+                            }
+                            else
+                            {
+                                MessageBox.Show("Bir hata oluştu. Yeni yazar eklenemedi!");
+                            }
+
+
+                            //baglantı kapanacak metot çağıralım
+                            BaglantiyiKapat();
+
+
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+
+                        MessageBox.Show("Ekleme işleminde beklemek hata oldu!" + ex.Message);
+                    }
+                    Temizle();
+                    break;
+                case "GÜNCELLE":
+                    try
+                    {
+                        if (!string.IsNullOrEmpty(txtYazar.Text))
+                        {
+                            using (baglanti)
+                            {
+                                DataGridViewRow satir = dataGridViewYazarlar.SelectedRows[0];
+                                int YazarID = Convert.ToInt32(satir.Cells["YazarID"].Value);
+                                //1.yol
+                                string updateSorguCumlesi = $"Update Yazarlar Set YazarAdSoyad='{txtYazar.Text.Trim()}' where YazarID={YazarID}";
+
+                                SqlCommand updateCommand = new SqlCommand(updateSorguCumlesi, baglanti);
+                                BaglantiyiAc();
+
+                                int sonuc = updateCommand.ExecuteNonQuery();
+                                if (sonuc > 0)
+                                {
+                                    MessageBox.Show($"Yazar güncellendi");
+                                    TumYazarlariGetir();
+                                }
+                                else
+                                {
+                                    MessageBox.Show("Yazar güncellenmedi!");
+                                }
+                            }
+                        }
+                        else
+                        {
+                            MessageBox.Show("Yazar adı olmadan güncelleme yapamazsın!");
+                        }
+
+
+                    }
+                    catch (Exception ex)
+                    {
+
+                        MessageBox.Show("Güncelleme işleminde beklenmedik bir hata oluştu!");
+                    }
+                    Temizle();
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void Temizle()
+        {
+            btnEkle.Text = "EKLE";
+            txtYazar.Clear();
+        }
+
+        private void BaglantiyiKapat()
+        {
+            try
+            {
+                if (baglanti.State != ConnectionState.Closed)
+                {
+                    baglanti.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show("Bağlantıyı kapatırken bir hata oldu! " + ex.Message);
+            }
+        }
+
+        private void BaglantiyiAc()
+        {
+            try
+            {
+                // bağlantı açık değilse açalım
+                if (baglanti.State != ConnectionState.Open)
+                {
+                    baglanti.ConnectionString = SQLBaglantiCumlesi;
+                    baglanti.Open();
+                }
+
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show("Bağlantı açılırken bir hata oluştu!" + ex.Message);
+            }
+        }
+
+        private string TarihiDuzenle(DateTime tarih)
+        {
+            string tarihString = string.Empty;
+            if (tarih != null)
+            {
+                tarihString = tarih.Year + "-" + tarih.Month + "-" + tarih.Day + " " + tarih.Hour + ":" + tarih.Minute + ":" + tarih.Second;
+            }
+            return tarihString;
+        }
+
+        private void guncelleToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //Seçili olan satırdaki istediğiniz hücreden(kolondan) değer aldık.
+            if (dataGridViewYazarlar.SelectedRows.Count > 0)
+            {
+                DataGridViewRow satir = dataGridViewYazarlar.SelectedRows[0];
+                string yazarAdSoyad = Convert.ToString(satir.Cells["YazarAdSoyad"].Value);
+                btnEkle.Text = "GÜNCELLE";
+                txtYazar.Text = yazarAdSoyad;
+
+                //kısa olsun isterseniz
+                //txtYazar.Text = COnvert.ToString(satir.Cells["YazarAdSoyad"].Value);
+            }
+            else
+            {
+                MessageBox.Show("");
+            }
+
+        }
+
+        private void btnTemizle_Click(object sender, EventArgs e)
+        {
+            Temizle();
+        }
+
+        private void silToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            DataGridViewRow secilensatir = dataGridViewYazarlar.SelectedRows[0];
+            int yazarID = (int)secilensatir.Cells["YazarID"].Value;
+            string yazar = Convert.ToString(secilensatir.Cells["YazarAdSoyad"].Value);
+
+            //Yazarın kitapları varsa Kitaplar tablosunda YazarID foregin key vardır.
+            //Bu durumda silme işlemi yapılmamalıdır.
+            //Varsa silmesine izin vermeyeceğiz.
+            //Yoksa silmek ister misin diye son bir kez sorup evet derse sileceğiz.
+
+            SqlCommand komut = new SqlCommand($"select * from Kitaplar where YazarID={yazarID}", baglanti);
+
+            komut.Connection = baglanti;
+            SqlDataAdapter adaptor = new SqlDataAdapter(komut); //adaptöre işleyeceği komutu adaptorun constructor'ında verdik.
+
+            DataTable sanalTablo = new DataTable();
+            BaglantiyiAc();
+            adaptor.Fill(sanalTablo);
+            if (sanalTablo.Rows.Count > 0)
+            {
+                MessageBox.Show($"{yazar} adlı yazarın Kitaplar tablosunda {sanalTablo.Rows.Count.ToString()} adet kitabı bulunmaktadır. Bu yazarı silmek için öncelikle sistemdeki ona tanımlı kitapları silmeni gerekmetedir. Litfen Kitap İşlemleri sayfasına gidiniz!");
+            }
+            else
+            {
+                //kitabı yok. Foreign key patlaması olmaz. Silebiliriz.
+
+
+
+                DialogResult cevap = MessageBox.Show($"{yazar} adlı yazarı silmek istediğinize emin misiniz?", "ONAY", MessageBoxButtons.YesNoCancel, MessageBoxIcon.Question);
+                if (cevap==DialogResult.Yes)
+                {
+                    //silecek
+                    //komut.CommandText = $"Delete from Yazarlar where YazarID={yazarID}";
+
+                    //@yzrid diyerek bir parametre oluşturmuş olduk.
+                    komut.CommandText = $"Delete from Yazarlar where YazarID=@yzrid";
+                    komut.Parameters.Clear();
+                    //AddWithValue metodu @yzrid yerine yazarID değerine sqlcommand nesnesinin commendText'inde bulunan sorgu cümlesine entegre eder.
+                    komut.Parameters.AddWithValue("@yzrid",yazarID);
+
+                    BaglantiyiAc();
+                    int sonuc = komut.ExecuteNonQuery();
+                    if (sonuc>0)
+                    {
+                        MessageBox.Show("Silindi");
+                        TumYazarlariGetir();
+                    }
+                    else
+                    {
+                        MessageBox.Show("HATA:Silinemedi!");
+                    }
+                }
+            }
+
+
+        }
+
+        private void silPasifeCekToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //Kullanıcı silindi zannedecek ama biz aslında psif yapıcağız
+            try
+            {
+                using (baglanti)
+                {
+                    DataGridViewRow satir = dataGridViewYazarlar.SelectedRows[0];
+                    int YazarID = Convert.ToInt32(satir.Cells["YazarID"].Value);
+                    //1.yol
+                    //string updateSorguCumlesi = $"Update Yazarlar Set SilindiMi=1 where YazarID={YazarID}";
+                    //2.yol
+                    string updateSorguCumlesi = $"Update Yazarlar Set SilindiMi=1 where YazarID=@yzrid";
+
+                    SqlCommand updateCommand = new SqlCommand(updateSorguCumlesi, baglanti);
+                    updateCommand.Parameters.Clear();
+                    updateCommand.Parameters.AddWithValue("@yzrid",YazarID);
+
+                    BaglantiyiAc();
+
+                    int sonuc = updateCommand.ExecuteNonQuery();
+                    if (sonuc > 0)
+                    {
+                        MessageBox.Show($"Yazar silindi");
+                        TumYazarlariGetir();
+                    }
+                    else
+                    {
+                        MessageBox.Show("DİKKAT: Yazar SİLİNMAEDİ!");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show("Pasife çek silme işeleminde hata: "+ex.Message);
+            }
+        }
+
+        private void silBaskaBirYontemToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+            // Bu yöntem yukarıdakiler gibi kullanışlı değildir.
+            try
+            {
+
+                DataGridViewRow secilenSatir = dataGridViewYazarlar.SelectedRows[0];
+                int yazarId = (int)secilenSatir.Cells["YazarId"].Value;
+                string yazar = Convert.ToString(secilenSatir.Cells["YazarAdSoyad"].Value);
+                SqlCommand komut = new SqlCommand();
+                komut.Connection = baglanti;
+                DialogResult cevap = MessageBox.Show($"{yazar} adlı yazarı silmek istediğinize emin misiniz?", "ONAY", MessageBoxButtons.YesNoCancel, MessageBoxIcon.Question);
+                if (cevap == DialogResult.Yes)
+                {
+                    komut.CommandText = $"Delete from Yazarlar where YazarId=@yzrid";
+                    komut.Parameters.Clear();
+                    komut.Parameters.AddWithValue("@yzrid", yazarId);
+                    BaglantiyiAc();
+                    int sonuc = komut.ExecuteNonQuery();
+                    if (sonuc > 0)
+                    {
+                        MessageBox.Show("Silindi");
+                        TumYazarlariGetir();
+                    }
+                    else
+                    {
+                        MessageBox.Show("HATA:Silinemedi!");
+                    }
+                    BaglantiyiKapat();
+                }
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show("HATA: " + ex.Message);
+            }
+        }
+    }
+}
