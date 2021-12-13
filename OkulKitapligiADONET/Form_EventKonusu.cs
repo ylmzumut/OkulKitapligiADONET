13a7042f9f21f7fbccb0f6aa655a12da462dcbf0
Umut Yılmaz <umut.yilmaz1@mf.karaelmas.edu.tr>
2021-12-13 15:08:14 +0300

    v2

---
+++ OkulKitapligiADONET/Form_EventKonusu.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OkulKitapligiADONET
+{
+    public partial class Form_EventKonusu : Form
+    {
+        public Form_EventKonusu()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_FareUzerineGelinceRenkDegissin(object sender, EventArgs e)
+        {
+            //button1.BackColor = Color.Pink;
+            //button2.BackColor = Color.Pink;
+
+            //Ya çok daha fazla button varsa?
+            foreach (var item in this.Controls)
+            {
+                if (item is Button && ((Button)sender).Name==((Button)item).Name)
+                {
+                    ((Button)item).BackColor = Color.PaleGoldenrod;
+                }
+            }
+        }
+        private void btn_FareAyrilincaDefaultRengeDonsun(object sender, EventArgs e)
+        {
+            //button1.BackColor = Color.Pink;
+            //button2.BackColor = Color.Pink;
+
+            //Ya çok daha fazla button varsa?
+            foreach (var item in this.Controls)
+            {
+                if (item is Button && ((Button)sender).Name == ((Button)item).Name)
+                {
+                    ((Button)item).BackColor = SystemColors.Control;
+                }
+            }
+        }
+
+        private void Form_EventKonusu_Load(object sender, EventArgs e)
+        {
+            //buttonların MouseLeave event'larını properties ekranındaki event sekmesinden değil Form'un load'ında ko ekranında biz verebiliriz.
+            //button1.MouseLeave += new EventHandler(btn_FareAyrilincaDefaultRengeDonsun);
+            //button2.MouseLeave += new EventHandler(btn_FareAyrilincaDefaultRengeDonsun);
+            //button3.MouseLeave += new EventHandler(btn_FareAyrilincaDefaultRengeDonsun);
+            //button4.MouseLeave += new EventHandler(btn_FareAyrilincaDefaultRengeDonsun);
+            foreach (var item in this.Controls)
+            {
+                if (item is Button)
+                {
+                    ((Button)item).MouseLeave += new EventHandler(btn_FareAyrilincaDefaultRengeDonsun);
+                }
+            }
+        }
+    }
+}
--- OkulKitapligiADONET/Program.cs
+++ OkulKitapligiADONET/Program.cs
@@ -17,7 +17,7 @@ namespace OkulKitapligiADONET
             Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new FormYazarlar());
+            Application.Run(new Form_EventKonusu());
         }
     }
 }
